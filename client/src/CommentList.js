aa02158da129da7252f9e6dab5e07a28a43ad0f4
Jayabrata Pramanik <jayabrata180402@gmail.com>
2023-07-13 00:43:11 +0530

    first all services

---
+++ client/src/CommentList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
---
+++ query/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+  req.setEncoding({});
+});
+
+app.listen(4002, () => {
+  console.log("query listening on 4002");
+});
